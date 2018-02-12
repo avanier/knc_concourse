fb10abc36a8fd8c1bc0a09f2ba60a7aa53ff72b2
Alexis Vanier <alexis@amonoid.io>
2018-02-12 00:33:15 -0500

    smarter firewall logic

--- recipes/firewall.rb
+++ recipes/firewall.rb
@@ -4,26 +4,30 @@ node.default['firewall']['allow_ssh'] = true
 
 include_recipe 'firewall::default'
 
-firewall_rule 'concourse-garden' do
-  protocol  :tcp
-  port      node['concourse']['worker']['CONCOURSE_GARDEN_BIND_PORT']
-  command   :allow
+if node['concourse']['run_worker']
+  firewall_rule 'concourse-garden' do
+    protocol  :tcp
+    port      node['concourse']['worker']['CONCOURSE_GARDEN_BIND_PORT']
+    command   :allow
+  end
+
+  firewall_rule 'concourse-baggageclaim' do
+    protocol  :tcp
+    port      node['concourse']['worker']['CONCOURSE_BAGGAGECLAIM_BIND_PORT']
+    command   :allow
+  end
 end
 
-firewall_rule 'concourse-baggageclaim' do
-  protocol  :tcp
-  port      node['concourse']['worker']['CONCOURSE_BAGGAGECLAIM_BIND_PORT']
-  command   :allow
-end
+if node['concourse']['run_web']
+  firewall_rule 'concourse-atc' do
+    protocol  :tcp
+    port      node['concourse']['web']['CONCOURSE_BIND_PORT']
+    command   :allow
+  end
 
-firewall_rule 'concourse-atc' do
-  protocol  :tcp
-  port      node['concourse']['web']['CONCOURSE_BIND_PORT']
-  command   :allow
-end
-
-firewall_rule 'concourse-tsa' do
-  protocol  :tcp
-  port      node['concourse']['web']['CONCOURSE_TSA_BIND_PORT']
-  command   :allow
+  firewall_rule 'concourse-tsa' do
+    protocol  :tcp
+    port      node['concourse']['web']['CONCOURSE_TSA_BIND_PORT']
+    command   :allow
+  end
 end
