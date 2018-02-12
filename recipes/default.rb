6a635c390c1c58c045b06fc1eb51f9dee6b81b76
Alexis Vanier <alexis@amonoid.io>
2018-02-11 22:11:41 -0500

    this actually works

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-unless node['family'] == 'redhat'
-  raise 'this cookbook is only compatible with RHEL/CentOS/Fedora'
+unless node['platform_family'] == 'rhel'
+  raise "this cookbook is only compatible with RHEL/CentOS/Fedora\nyour OS is #{node['family']}"
 end
 
 include_recipe '::binary'
