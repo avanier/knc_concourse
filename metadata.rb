1e9021577fcc4f2be795be156648914ae3787028
Alexis Vanier <alexis@amonoid.io>
2018-02-17 01:11:59 -0500

    v1.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alexis@amonoid.io'
 license 'Apache-2.0'
 description 'Installs/Configures knc_concourse'
 long_description 'Installs/Configures knc_concourse'
-version '1.0.1'
+version '1.0.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 source_url 'https://github.com/avanier/knc_concourse'
