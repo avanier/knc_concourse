a29abe7c01012ad1bc07565cc2e9a2476d6e9042
Alexis Vanier <alexis@amonoid.io>
2018-02-25 17:28:38 -0500

    Remove CONCOURSE_BAGGAGECLAIM_VOLUMES and sort keys

--- attributes/concourse-worker.rb
+++ attributes/concourse-worker.rb
@@ -4,9 +4,8 @@ default['concourse']['worker']['data_bag'] = 'concourse/worker-secrets'
 
 default['concourse']['worker']['CONCOURSE_BAGGAGECLAIM_BIND_PORT'] = 7788
 default['concourse']['worker']['CONCOURSE_BAGGAGECLAIM_DRIVER'] = 'btrfs'
-default['concourse']['worker']['CONCOURSE_BAGGAGECLAIM_VOLUMES'] = '/srv/concourse/volumes'
 default['concourse']['worker']['CONCOURSE_GARDEN_BIND_PORT'] = 7777
 default['concourse']['worker']['CONCOURSE_NAME'] = node['fqdn']
 default['concourse']['worker']['CONCOURSE_TSA_PUBLIC_KEY'] = '/etc/concourse/keys/web/public'
-default['concourse']['worker']['CONCOURSE_WORK_DIR'] = '/srv/concourse/work'
 default['concourse']['worker']['CONCOURSE_TSA_WORKER_PRIVATE_KEY'] = '/etc/concourse/keys/worker/private'
+default['concourse']['worker']['CONCOURSE_WORK_DIR'] = '/srv/concourse'
